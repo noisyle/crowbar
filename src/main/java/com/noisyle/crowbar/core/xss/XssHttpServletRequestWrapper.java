938bb727a438ee74c4d7a6d8c390d0927b9e302d
noisyle <anyone.1984@gmail.com>
2015-10-17 21:40:21 +0800

    fix bug in xss-filter

--- src/main/java/com/noisyle/crowbar/core/xss/XssHttpServletRequestWrapper.java
+++ src/main/java/com/noisyle/crowbar/core/xss/XssHttpServletRequestWrapper.java
@@ -38,13 +38,13 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
 	private String cleanXSS(String value) {
 		// You'll need to remove the spaces from the html entities below
-		value = value.replaceAll("</?script>", "");
 		value = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 		value = value.replaceAll("\\(", "&#40;").replaceAll("\\)", "&#41;");
 		value = value.replaceAll("'", "&#39;");
 		value = value.replaceAll("\"", "&quot;");
 		value = value.replaceAll("eval\\((.*)\\)", "");
 		value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+		value = value.replaceAll("&lt;/?script&gt;", "");
 //		value = value.replaceAll("script", "");
 		return value;
 	}
